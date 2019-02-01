000ac5b44ee0fce3aa89732b36623d3ea9ed7248
Bernhard Miettinen <benhard.miettinen@gmail.com>
2019-02-01 22:48:37 +0200

    initial commit

---
+++ Assets/Hirame/SceneComposer/Runtime/SubScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Hirame.SceneComposer
+{
+    [System.Serializable]
+    public sealed class SubScene
+    {
+
+        public string assetGuid;
+    }
+}
